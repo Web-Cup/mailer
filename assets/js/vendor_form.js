a97bb758355b903a73c263571b701614cf1facd9
VHK0 <smart_mine@hotmail.com>
2020-09-01 12:02:30 +0800

    Mail sending mailler

---
+++ assets/js/vendor_form.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('#ThanksModal').modal('show');
+});
---
+++ index.php
@@ -0,0 +1,242 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Libertyfriedman</title>
+
+    <!-- Favicon Icon -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/png">
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- Fontawesome css -->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+
+    <!-- Plugins css -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!-- Utilities css -->
+    <link rel="stylesheet" href="assets/css/Util.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+</head>
+
+<body>
+
+    <div class="viewport-wrapper">
+        <img src="assets/img/tt-1.png" alt="" class="transparent-img tt-1">
+        <img src="assets/img/tt-2.png" alt="" class="transparent-img tt-2">
+        <img src="assets/img/tt-3.png" alt="" class="transparent-img tt-3">
+
+
+        <!-- all popup MarkUp Start  -->
+        <!-- Popup Markup for About Menu Start  -->
+
+        <div class="modal fade" id="exampleModalAbout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>About</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <div class="about-m-content-wrapper">
+                            <p>Liberty Friedman, LLC is an impact-driven global macro investment manager. We aim to generate market-beating returns through investment themes across asset classes that dismantle systems of human oppression and restore our environment. </p>
+                            <h5 class="tertiary-title">Investment Thesis</h5>
+                            <p>Markets are imperfect. They reflect the values, biases, and blindnesses of all participants, especially those who control the capital. Where there is systemic bias, we see opportunity for investment, growth, and cascading returns. </p>
+
+                            <div class="pf-links">
+                                <a href="#">Chid Liberty, Managing Partner - LinkedIn</a>
+                                <a href="#">Anne Friedman, Managing Partner - LinkedIn</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Popup Markup for About Menu End  -->
+
+        
+        <!-- Popup Markup for Funds Menu Start  -->
+        
+        <div class="modal fade" id="exampleModalFunds" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>Funds</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <div class="fund-m-content-wrapper">
+                            <p>Liberty Friedman Fund I, LP</p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Popup Markup for Funds Menu End  -->
+
+        
+        <!-- Popup Markup for Contact Menu Start  -->
+        
+        <div class="modal fade" id="exampleModalContact" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>Contact</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <form action=""  method="post">
+                            <div class="input-inside">
+                                <input type="text" name="Name" required placeholder="Name">
+                            </div>
+                            <div class="input-inside">
+                                <input type="email" name="Email" required placeholder="Email">
+                            </div>
+                            <div class="input-inside">
+                                <input type="text" name="Subject" required placeholder="Subject">
+                            </div>
+                            <div class="input-inside">
+                                <textarea name="comment" required id="comment" cols="30" rows="10" placeholder="Comment"></textarea>
+                            </div>
+                            <div class="submit-button">
+                                <button type="submit" name="SendEmail">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Popup Markup for Contact Menu End  -->
+        <!-- all popup MarkUp End -->
+        <div class="modal fade" id="ThanksModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered menu-popup" role="document">
+            <div class="modal-content">
+                <div class="popup-title-wrap">
+                    <h3>Contact</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <div class="popup-description">
+                        <div class="fund-m-content-wrapper">
+                            <p>Your contact has been submitted.</p>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="viewport-content-box">
+            <h1 class="viewport-title">an impact-driven global macro hedge fund</h1>
+            <div class="door-img">
+                <img src="assets/img/door.png" alt="">
+            </div>
+            <div class="viewport-btm-content">
+                <ul class="main-menu">
+                    <li><a href="#" data-toggle="modal" data-target="#exampleModalAbout">About</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#exampleModalFunds">Funds</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#exampleModalContact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div> <!-- ./viewport-wrapper -->
+
+    <!-- Jquery Js -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once "vendor/autoload.php";
+
+
+if (isset($_POST['SendEmail'])) {
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $Subject = $_POST['Subject'];
+    $Content = $_POST['comment'];
+    //PHPMailer Object
+    $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+    
+    //From email address and name
+    $mail->From = $Email;
+    $mail->FromName = $Name;
+    
+    //To address and name
+    $mail->addAddress("smartmine227@gmail.com", "Recepient Name");
+    $mail->addAddress("smartmine227@gmail.com"); //Recipient name is optional
+    
+    //Address to which recipient will reply
+    $mail->addReplyTo("smartmine227@gmail.com", "Reply");
+    
+    //Send HTML or Plain Text email
+    $mail->isHTML(true);
+    
+    $mail->Subject = $Subject;
+    $mail->Body = "<i>".$Content."</i>";
+    $mail->AltBody = "This is the plain text version of the email content";
+
+    try {
+        $mail->send();
+        echo '<script src="assets/js/vendor_form.js" type="text/javascript"></script>';
+        $sessionValue = 'false';
+    } catch (Exception $e) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+}    
+
+
+      ?>
+          <!-- Bootstrap Js -->
+<script src="assets/js/bootstrap.min.js"></script>
+
+<!-- Plugins Js -->
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+<!-- Main Js -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
